f6d0725e17fc02db7082cbf5922ae5856dd7b74a
yxs <asong0122@163.com>
2021-10-28 22:20:00 +0800

    商品分类页面添加及功能实现

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,66 +1,43 @@
-// pages/category/index.js
+import { request } from "../../request/index"
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-
+    leftMenuList: [], // 左侧菜单数据
+    rightGoodList: [], // 右侧商品数据
+    currentIndex: 0 // 当前选中菜单下标
   },
-
+  cates: [], // 接口的返回数据
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.getCates()
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  // 获取分类数据
+  getCates() {
+    request({
+      url: 'categories'
+    }).then(data => {
+      this.cates = data
+      // 左侧菜单数据
+      let leftMenuList = this.cates.map(item => item.cat_name)
+      // 右侧商品数据
+      let rightGoodList = this.cates[0].children
+      this.setData({
+        leftMenuList,
+        rightGoodList
+      })
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 左侧菜单点击事件
+  handleMenuClick(e) {
+    const {index} = e.currentTarget.dataset
+    let rightGoodList = this.cates[index].children
+    this.setData({
+      currentIndex: index,
+      rightGoodList
+    })
   }
 })
