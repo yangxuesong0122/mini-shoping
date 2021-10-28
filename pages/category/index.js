d5de0df9a5c39ddfca9f008b7f1fe5a26d37cdf2
yxs <asong0122@163.com>
2021-10-28 22:43:21 +0800

    商品分类数据进行本地存储

--- pages/category/index.js
+++ pages/category/index.js
@@ -13,7 +13,41 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.getCates()
+    /* 
+    web中的本地存储和小程序中的本地存储的区别
+      1 写代码的方式不一样 
+        web: localStorage.setItem("key","value") localStorage.getItem("key")
+    小程序中: wx.setStorageSync("key", "value"); wx.getStorageSync("key");
+      2:存的时候 有没有做类型转换
+        web: 不管存入的是什么类型的数据，最终都会先调用一下 toString(),把数据变成了字符串 再存入进去
+        小程序: 不存在类型转换这个操作，存什么类型的数据进去，获取的时候就是什么类型
+    1 先判断一下本地存储中有没有旧的数据
+      {time:Date.now(),data:[...]}
+    2 没有旧数据 直接发送新请求 
+    3 有旧的数据 同时 旧的数据也没有过期 就使用 本地存储中的旧数据即可
+     */
+    //  1 获取本地存储中的数据 (小程序中也是存在本地存储技术)
+    const cates = wx.getStorageSync("cates");
+    // 2 判断
+    if (!cates) {
+      // 不存在  发送请求获取数据
+      this.getCates();
+    } else {
+      // 有旧的数据 定义过期时间
+      if (Date.now() - cates.time > 1000 * 10) {
+        // 重新发送请求
+        this.getCates();
+      } else {
+        // 可以使用旧的数据
+        this.cates = cates.data;
+        let leftMenuList = this.cates.map(v => v.cat_name);
+        let rightGoodList = this.cates[0].children;
+        this.setData({
+          leftMenuList,
+          rightGoodList
+        })
+      }
+    }
   },
   // 获取分类数据
   getCates() {
@@ -21,6 +55,8 @@ Page({
       url: 'categories'
     }).then(data => {
       this.cates = data
+      // 把接口的数据存入到本地存储中
+      wx.setStorageSync("cates", { time: Date.now(), data: this.cates });
       // 左侧菜单数据
       let leftMenuList = this.cates.map(item => item.cat_name)
       // 右侧商品数据
