f57a3d32c604baa59153f5b82e2c22255eae8866
yxs <asong0122@163.com>
2021-10-31 11:28:32 +0800

    搜索页面实现,实现防抖

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,66 +1,45 @@
-// pages/search/index.js
-Page({
+/* 
+1 输入框绑定 值改变事件 input事件
+  1 获取到输入框的值
+  2 合法性判断 
+  3 检验通过 把输入框的值 发送到后台
+  4 返回的数据打印到页面上
+2 防抖 （防止抖动） 定时器  节流 
+  0 防抖 一般输入框中防止重复输入重复发送请求
+  1 节流 一般是用在页面下拉和上拉 
+  1 定义全局的定时器id
+ */
+import { request } from "../../request/index"
 
+Page({
   /**
    * 页面的初始数据
    */
   data: {
-
+    goods: []
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  timeId: '',
+  handleInput(e) {
+    // 获取输入框的值
+    const {value} = e.detail
+    // 检查合法性
+    if (!value.trim()) {
+      return
+    }
+    // 获取数据
+    clearTimeout(this.timeId)
+    this.timeId = setTimeout(() => {
+      this.getSearchGoods(value)
+    }, 1000)
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 获取数据
+  async getSearchGoods(query) {
+    const {goods} = await request({
+      url: 'goods/search',
+      data: {query}
+    })
+    this.setData({
+      goods
+    })
   }
 })
